a120bc7b7991c10062bfab5f0118bec3d1a41e36
Juan Camilo Salazar Restrepo <juancamilosalazarrestrepo@gmail.com>
2026-02-26 20:19:39 -0500

    Make Supabase browser client a singleton and add error boundary

--- src/lib/supabase-browser.ts
+++ src/lib/supabase-browser.ts
@@ -1,8 +1,15 @@
 import { createBrowserClient } from '@supabase/ssr';
+import { SupabaseClient } from '@supabase/supabase-js';
+
+let supabaseClient: SupabaseClient | undefined;
 
 export function createSupabaseBrowser() {
-    return createBrowserClient(
+    if (supabaseClient) return supabaseClient;
+
+    supabaseClient = createBrowserClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     );
+
+    return supabaseClient;
 }
